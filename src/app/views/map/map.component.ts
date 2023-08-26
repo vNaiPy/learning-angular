151d8777d6fb1e49e92f63f3a0ea02138efe3ffe
Handrei Morais <handrei.m.souza@hotmail.com>
2023-08-26 10:58:23 -0300

    bkp para o pc 2
    bkp para o pc 2

--- src/app/views/map/map.component.ts
+++ src/app/views/map/map.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import VectorSource from 'ol/source/Vector.js';
-import Point from 'ol/geom/Point.js';
-import Feature from 'ol/Feature.js';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import {Group, Vector as VectorLayer} from 'ol/layer.js';
-import TileLayer from 'ol/layer/Tile';
-import {fromLonLat} from 'ol/proj.js';
-import OSM from 'ol/source/OSM';
-import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import mapboxgl from 'mapbox-gl';
 
 
 @Component({
@@ -20,89 +11,51 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 export class MapComponent implements OnInit {
 
   title = 'alpha-front';
-  map!: Map
-  map_url = "";
   latitude = 0;
   longitude = 0;
-  mapt!: mapboxgl.Map
+  map!: mapboxgl.Map
 
   ngOnInit() {
-    //this.verifica_local();
+    this.takeUserLocation()
     this.mapInitializer();
-
   }
 
-  mapInitializer () {
-  if (navigator.geolocation) {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiaGFuZHJlaW1vcmFpcyIsImEiOiJjbGxoZnZ4dzQwYXhkM2xsdzZ2aW9uc2tyIn0.0u2hw87FzG6EmZ5Dz3z0PA';
-    this.mapt = new mapboxgl.Map({
-      container: 'map-box',
-      style:  'mapbox://styles/handreimorais/cllhg626n020f01mf64t075eb'
-    });
-    this.mapt.addControl(new mapboxgl.NavigationControl())
-    this.mapt.addControl(new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl
-    }),
-    "top-left")
-    this.mapt.addControl(new mapboxgl.GeolocateControl())
-  }
-  else alert("É necessário habilitar sua localização para usar os recursos deste app!")
-  }
-
-  verifica_local() {
+  takeUserLocation () {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-      this.latitude = -23.6509822
-      this.longitude = -46.5746165
-      console.log(this.latitude)
-      console.log(this.longitude)
-      this.mapInit(this.longitude, this.latitude)},
-      () => { alert("Sem localização!") },
-      { timeout: 15000 });
+      navigator.geolocation.getCurrentPosition((userPosition) => {
+        this.latitude = userPosition.coords.latitude
+        this.longitude = userPosition.coords.longitude
+        console.log(this.latitude)
+        console.log(this.longitude)
+      })
     }
     else alert("Sem permissão!")
   }
 
-  mapInit (longitude: number, latitude: number) {
-    const myPoint = new Feature({geometry: new Point(fromLonLat([longitude, latitude])),})
-    const vectorSource = new VectorSource({features: [myPoint],})
-    const vectorLayer = new VectorLayer(
-      {source: vectorSource,
-      style: {
-        'fill-color': 'rgba(255, 255, 255, 0.2)',
-        'stroke-color': '#ffcc33',
-        'stroke-width': 2,
-        'circle-radius': 7,
-        'circle-fill-color': '#ffcc33',
-      }}
-      )
+  mapInitializer () {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiaGFuZHJlaW1vcmFpcyIsImEiOiJjbGxoZnZ4dzQwYXhkM2xsdzZ2aW9uc2tyIn0.0u2hw87FzG6EmZ5Dz3z0PA';
+    this.map = new mapboxgl.Map({
+      container: 'map-box',
+      style:  'mapbox://styles/handreimorais/cllhg626n020f01mf64t075eb'
+    });
 
-    let mapView =  new View({
-        center: fromLonLat([longitude, latitude]),
-        zoom: 16,
-      })
+    let mapBoxGeocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      },)
+    this.map.addControl(new mapboxgl.NavigationControl())
+    this.map.addControl(new mapboxgl.GeolocateControl())
+    this.map.addControl(mapBoxGeocoder, "top-left");
 
-    this.map = new Map({
-      target: 'ol-map'
-    })
-    this.map.setView(mapView)
+    let marker1 = new mapboxgl.Marker()
 
-    const openStreetMapStandard = new TileLayer ({
-      source: new OSM(),
-      visible: true
-    })
-
-    let layerGroup = new Group({
-      layers: [
-        openStreetMapStandard, vectorLayer
-      ]
-    })
-
-    this.map.addLayer(layerGroup)
-
-    this.map.on('click', function(ev) {
-      console.log(ev.coordinate)
+    this.map.on('click', (e) => {
+        this.longitude = e.lngLat.lng
+        this.latitude = e.lngLat.lat
+        console.log(this.longitude)
+        console.log(this.latitude)
+        marker1.setLngLat([this.longitude, this.latitude])
+        .addTo(this.map)
     })
   }
 
